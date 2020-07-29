c25c6bcb42c7132e9882667ac3c9470e6f89c3be
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2020-07-29 16:00:37 +0200

    Add files via upload

--- course_project/queries.py
+++ course_project/queries.py
@@ -1,47 +1,54 @@
 qry_create_address_tbl = """
-            CREATE TABLE tblAddress (
-                id text,
-                country text,
-                city text,
-                street text,
-                postal text
-            )
-        """
+                            CREATE TABLE tblAddress (
+                                id text,
+                                country text,
+                                city text,
+                                street text,
+                                postal text
+                            )
+                        """
 
 qry_create_employee_tbl = """
-            CREATE TABLE tblEmployee (
-                id text,
-                firstName text,
-                lastName text,
-                phoneNumber text,
-                dateOfBirth text,
-                addresses text,
-                title text,
-                international integer,
-                dateOfEmployment text
-            )
-        """
+                            CREATE TABLE tblEmployee (
+                                id text,
+                                firstName text,
+                                lastName text,
+                                phoneNumber text,
+                                dateOfBirth text,
+                                addresses text,
+                                title text,
+                                international integer,
+                                dateOfEmployment text
+                            )
+                        """
 
 qry_create_trainer_tbl = """
-            CREATE TABLE tblTrainer (
-                id text,
-                firstName text,
-                lastName text,
-                phoneNumber text,
-                dateOfBirth text,
-                addresses text,
-                domain text,
-                salary integer,
-                courses text,
-                got_raise text
-            )
-        """
+                            CREATE TABLE tblTrainer (
+                                id text,
+                                firstName text,
+                                lastName text,
+                                phoneNumber text,
+                                dateOfBirth text,
+                                addresses text,
+                                domain text,
+                                salary integer,
+                                courses text,
+                                got_raise text
+                            )
+                        """
 
 qry_create_courses_tbl = """
-            CREATE TABLE tblCourse (
-                id text,
-                code text,
-                min integer,
-                max integer
-            )
-        """
+                            CREATE TABLE tblCourse (
+                                id text,
+                                code text,
+                                min integer,
+                                max integer
+                            )
+                        """
+
+qry_create_enrollment_tbl = """
+                            CREATE TABLE tblEnrollment (
+                                course text,
+                                employee text
+                            )
+                        """
